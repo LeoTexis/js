2691c6c24a5d930d8b225e2773d5dcbcf65fd708
LeoTexis <139823053+LeoTexis@users.noreply.github.com>
2023-09-10 08:59:47 -0300

    a

--- aula004/node_4bc106a466117.tmp.js
+++ aula004/node_4bc106a466117.tmp.js
@@ -1,17 +1,16 @@
 var c = 1
 do {
     console.log('Tudo bom?')
-    c++
 } while (c <= 6)
 
 
 
 
-/* var c = 1
+/*var c = 1
 while (c < 6) {
     console.log(`tudo bom?${c}`)
     c++
-} */
+}*/
 
 
 
