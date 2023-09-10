5c09a1c6ad9ed4bf5990c8bc76a4fa66d16e42a6
LeoTexis <139823053+LeoTexis@users.noreply.github.com>
2023-09-09 23:54:10 -0300

    Create ex016.js

---
+++ aula004/ex016.js
@@ -0,0 +1,15 @@
+var c = 1
+while (c <6) {
+    console.log(`tudo bom?${c}`)
+    c++
+}
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
