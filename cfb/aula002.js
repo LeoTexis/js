fa6bc752168b1dc5cfe1c8fcc6278077705f0bc0
LeoTexis <139823053+LeoTexis@users.noreply.github.com>
2023-09-16 00:17:44 -0300

    reforçando
    Treinando mais JS antes do nodeJS e database

---
+++ cfb/aula002.js
@@ -0,0 +1,9 @@
+/* strict mode
+deixar o codigo mais "limpo" com menos sujeira, menos coisa inutilizavel.
+exemplo : nesse modo nao conseguimos usar uma variavel que n foi declarada. MAIS CUIDADO NA HORA DE UTILIZAR E DECLARAR */
+
+"use strict"
+nome = "Leo"
+console.log("Eu amo bacon!")
+console.log(nome)
+console.log(nome + " diz que ama bacon!")
---
+++ cfb/aula003.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function teste() {
+    if(true){
+        let nome = "Leo"
+        console.log("dentro do teste if: " + nome)
+    }
+    console.log("dentro do teste : " + nome)
+}
+
+teste()
+
+console.log("fora do teste : " + nome)
