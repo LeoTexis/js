aae9a0d68ae9c1d9e84105de5f980072e46d6ae0
LeoTexis <139823053+LeoTexis@users.noreply.github.com>
2024-01-08 19:29:55 -0300

    lib filesystem (FS)

---
+++ alura/OO/objLiteral.js
@@ -0,0 +1,24 @@
+const user = {
+    nome: "Leonardo",
+    email: "ltl@mail.com",
+    nascimento: "2024/01/01",
+    role: "admin",
+    ativo: true,
+    exibirInfos: function() {
+        console.log(this.nome)
+    }
+}
+
+// user.exibirInfos()
+// const exibir = user.exibirInfos
+// exibir()
+
+const exibir = function() {
+    console.log(this.nome, this.email)
+}
+
+const exibirNome = exibir.bind(user)
+exibirNome()
+exibir()
+
+module.exports = user
