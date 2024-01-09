f7454a7a4ef7aabc9f6d315d8a06a5273d11b76e
LeoTexis <139823053+LeoTexis@users.noreply.github.com>
2024-01-08 21:29:37 -0300

    Update obj02.js

--- alura/OO/obj02.js
+++ alura/OO/obj02.js
@@ -1,2 +1,25 @@
-const user = require("./objLiteral")
+const user = {
+    nome: "Leonardo",
+    email: "ltl@mail.com",
+    nascimento: "2024/01/01",
+    role: "estudante",
+    ativo: true,
+    exibirInfos: function() {
+        console.log(this.nome, this.email)
+    }
+}   
 
+
+const admin = {
+    nome: "Iara",
+    email: "iarapoly@mail.com",
+    role: "adm",
+    criarCurso() {
+        console.log("curso criado!")
+    }
+}
+
+
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exibirInfos()
